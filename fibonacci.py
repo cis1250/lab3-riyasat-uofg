0d9991a3ca0d576325af410fd2d7f415df4fc358
riyasat-uofg <riyasat@uoguelph.ca>
2025-09-30 10:53:33 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+while True:
+    n = input("How many terms of the Fibonacci sequence do you want? ")
+    if n.isdigit() and int(n) > 0:
+        n = int(n)
+        break
+    else:
+        print("Please enter a positive integer.")
+
+# Print Fibonacci sequence
+a, b = 0, 1
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
+
+
+
 # Fibonacci Sequence Exercise
 # TODO: (Read detailed instructions in the Readme file)
 # Prompt the user for the number of terms.
